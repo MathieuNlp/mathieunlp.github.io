56d72a9783d719ecc0bfc5faeee93219014943a7
MathieuNlp <mathieunalpon@gmail.com>
2026-01-02 15:09:33 +0000

    Deploying to gh-pages from @ MathieuNlp/mathieunlp.github.io@071481f8c18596c43026a143ec19c797ac5e36e8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-notes",
+  },{id: "nav-notes",
           title: "notes",
           description: "research notes (continuous progress)",
           section: "Navigation",
